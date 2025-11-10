08c9f0b7cdc251386de2dc52dff7d5d9079d9a13
Hunter <hunterjfrey@gmail.com>
2025-11-10 08:04:04 -0600

    Trying to enable github pages for testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppThemeProvider } from "./theme/AppThemeProvider.tsx";
 import "./global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppThemeProvider>
         <App />
       </AppThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
